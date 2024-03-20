c8a3e2b971d47600ef647d6217a99daf27cbae13
sachinnautiyal-github <nautiyalsachin217@gmail.com>
2024-03-20 11:30:24 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/data', require('./routes/data'));
 
+app.use('/', (req, res)=>{
+    res.send("hello from server");
+})
+
 app.listen(5000, ()=>{
     console.log("server started listening at port 5000")
 })
